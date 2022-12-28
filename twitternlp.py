8c18fde3f07fe6301452f704c49531b4d6e70d98
orangescorpion <olivia.wass@bigpond.com>
2022-12-28 17:45:05 +1100

    broken in attempt to use holdou validation

--- twitternlp.py
+++ twitternlp.py
@@ -6,26 +6,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
-import scipy.sparse
 
 # Data load
 train = pd.read_csv("train.csv", index_col="id") # read training dataset
 test = pd.read_csv("test.csv", index_col="id") # read test dataset
-# Bag of words for training set
+
+# Bag of words and data partition
 count_vectorizer=feature_extraction.text.CountVectorizer() # from sklearn
 trainvectors = count_vectorizer.fit_transform(train["text"]) # vectorise the entire training set
-
-# print(type(trainvectors))
-# print(testvectors.shape)
-train_train, train_test = train_test_split(trainvectors, train_size=0.7, random_state=42) #split the dataset for CV
-# print(train_train.info())
-
+df = pd.DataFrame(trainvectors.toarray())
+df.info()
+df["target"]=train["target"]
+df.info()
+train_train, train_test = train_test_split(df, train_size=0.7, random_state=42) #split the dataset for holdout validation
 
 ### Data processing
 # TODO: consider capitalization because this will count as different words (but then again all caps might be significant)
 # TODO: consider stemming/lemmatization
 # TODO: remove punctuation?
-
 
 ### Model training
 lrdge=linear_model.RidgeClassifier() # Ridge Regression
@@ -36,9 +34,10 @@ knn=KNeighborsClassifier() # K Nearest Neighbours
 #naive = nltk.classify.NaiveBayesClassifier() # Naive Bayes
 train_targets = pd.Series.to_numpy(train["target"]) # creates a numpy array for comparison to predictions
 
+trainx = train_train[train_train.columns.difference(["target"])]
 # Ridge regression model fitting
-lrdge.fit(trainvectors, train["target"]) # fits model on training data
-lrdge_pred = lrdge.predict(trainvectors) # Makes predictions based on fitted model
+lrdge.fit(trainx, train_train["target"]) # fits model on training data
+lrdge_pred = lrdge.predict(train_test) # Makes predictions based on fitted model
 
 lrdge_accuracy = 0
 for x in lrdge_pred:

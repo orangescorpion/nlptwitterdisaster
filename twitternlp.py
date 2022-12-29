0e1ed589382db7a411d6d62f3063093f78d5071a
orangescorpion <olivia.wass@bigpond.com>
2022-12-29 11:00:12 +1100

    fixed data types for holdout validation

--- twitternlp.py
+++ twitternlp.py
@@ -15,9 +15,7 @@ test = pd.read_csv("test.csv", index_col="id") # read test dataset
 count_vectorizer=feature_extraction.text.CountVectorizer() # from sklearn
 trainvectors = count_vectorizer.fit_transform(train["text"]) # vectorise the entire training set
 df = pd.DataFrame(trainvectors.toarray())
-df.info()
-df["target"]=train["target"]
-df.info()
+df["target"]=pd.Series.to_numpy(train["target"])
 train_train, train_test = train_test_split(df, train_size=0.7, random_state=42) #split the dataset for holdout validation
 
 ### Data processing
@@ -32,13 +30,16 @@ rf=RandomForestClassifier() # Random Forest
 mlp=MLPClassifier() # multi layer perceptron classifier
 knn=KNeighborsClassifier() # K Nearest Neighbours
 #naive = nltk.classify.NaiveBayesClassifier() # Naive Bayes
-train_targets = pd.Series.to_numpy(train["target"]) # creates a numpy array for comparison to predictions
+train_targets = pd.Series.to_numpy(train_test["target"]) # creates a numpy array for comparison to predictions
 
-trainx = train_train[train_train.columns.difference(["target"])]
+trainx = train_train[train_train.columns.difference(["target"])] # Training data minus target
+train_testy = train_test[train_test.columns.difference(["target"])] # Test data minus target
+
 # Ridge regression model fitting
 lrdge.fit(trainx, train_train["target"]) # fits model on training data
-lrdge_pred = lrdge.predict(train_test) # Makes predictions based on fitted model
+lrdge_pred = lrdge.predict(train_testy) # Makes predictions based on fitted model
 
+# This can be a function
 lrdge_accuracy = 0
 for x in lrdge_pred:
     if lrdge_pred[x] == train_targets[x]:
@@ -47,6 +48,6 @@ lrdge_accuracy = (lrdge_accuracy/len(lrdge_pred))*100
 print("Ridge regression accuracy: "+str(lrdge_accuracy))
 
 # TODO: Model prediction on test data with best model
-
+# TODO: cross validation
 # TODO: Save final prediction as submission.csv
 

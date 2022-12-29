dd17d844a738f529ee95ab87110dcee1244a6009
orangescorpion <olivia.wass@bigpond.com>
2022-12-29 11:08:25 +1100

    added function to calculate accuracy

--- twitternlp.py
+++ twitternlp.py
@@ -11,17 +11,19 @@ from sklearn.neighbors import KNeighborsClassifier
 train = pd.read_csv("train.csv", index_col="id") # read training dataset
 test = pd.read_csv("test.csv", index_col="id") # read test dataset
 
+### Data processing
+# TODO: consider capitalization because this will count as different words (but then again all caps might be significant)
+# TODO: consider stemming/lemmatization
+# TODO: remove punctuation?
+
 # Bag of words and data partition
 count_vectorizer=feature_extraction.text.CountVectorizer() # from sklearn
 trainvectors = count_vectorizer.fit_transform(train["text"]) # vectorise the entire training set
 df = pd.DataFrame(trainvectors.toarray())
 df["target"]=pd.Series.to_numpy(train["target"])
 train_train, train_test = train_test_split(df, train_size=0.7, random_state=42) #split the dataset for holdout validation
-
-### Data processing
-# TODO: consider capitalization because this will count as different words (but then again all caps might be significant)
-# TODO: consider stemming/lemmatization
-# TODO: remove punctuation?
+trainx = train_train[train_train.columns.difference(["target"])] # Training data minus target
+train_testy = train_test[train_test.columns.difference(["target"])] # Test data minus target
 
 ### Model training
 lrdge=linear_model.RidgeClassifier() # Ridge Regression
@@ -32,20 +34,18 @@ knn=KNeighborsClassifier() # K Nearest Neighbours
 #naive = nltk.classify.NaiveBayesClassifier() # Naive Bayes
 train_targets = pd.Series.to_numpy(train_test["target"]) # creates a numpy array for comparison to predictions
 
-trainx = train_train[train_train.columns.difference(["target"])] # Training data minus target
-train_testy = train_test[train_test.columns.difference(["target"])] # Test data minus target
+def accuracy(predicted, actual): # Function to return accuracy of predicted to actual, TODO: false positives / negatives
+    accuracy = 0
+    for x in predicted:
+        if predicted[x] == actual[x]:
+            accuracy += 1
+    accuracy = (accuracy/len(predicted))*100
+    return accuracy
 
 # Ridge regression model fitting
 lrdge.fit(trainx, train_train["target"]) # fits model on training data
 lrdge_pred = lrdge.predict(train_testy) # Makes predictions based on fitted model
-
-# This can be a function
-lrdge_accuracy = 0
-for x in lrdge_pred:
-    if lrdge_pred[x] == train_targets[x]:
-        lrdge_accuracy += 1
-lrdge_accuracy = (lrdge_accuracy/len(lrdge_pred))*100
-print("Ridge regression accuracy: "+str(lrdge_accuracy))
+print("Ridge regression accuracy: "+str(accuracy(lrdge_pred, train_targets))) # Print accuracy for model
 
 # TODO: Model prediction on test data with best model
 # TODO: cross validation
